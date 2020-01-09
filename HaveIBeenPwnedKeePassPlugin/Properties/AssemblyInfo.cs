37e89cdea0bcb5d01a7628ce8c7aecf858a8de4f
kapsiR <kapsiR@users.noreply.github.com>
2020-01-10 00:01:02 +0100

    Add git based versioning

--- HaveIBeenPwnedKeePassPlugin/Properties/AssemblyInfo.cs
+++ HaveIBeenPwnedKeePassPlugin/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("kapsiR")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyCopyright("Copyright © 2019 kapsiR")]
+[assembly: AssemblyCopyright("Copyright © 2020 kapsiR")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,16 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cac070c1-8b7e-4ba7-8f20-b3e4f56d2e03")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
